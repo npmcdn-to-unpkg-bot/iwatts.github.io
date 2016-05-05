3d85575f355b55606f8b793973258e106f48329b
Isaac <theicean@gmail.com>
2016-05-05 10:29:50 -0600

    loops

---
+++ src/loop.js
@@ -0,0 +1,55 @@
+/**
+ * Created by isaacwatts on 5/5/16.
+ */
+
+console.log("Ready");
+
+var listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (var i = 0; i < 10; i++) {
+    //console.log(listOfNumbers[i]);
+}
+//console.log("--------------------------------------");
+for (var i = 0; i < listOfNumbers.length; i++) {
+    //listOfNumbers[i] = "My content: " + i;
+    //console.log(listOfNumbers[i]);
+}
+//console.log("--------------------------------------");
+for (var i = listOfNumbers.length; i > 0; i--) {
+    var index = i - 1;
+    //console.log(listOfNumbers[index]);
+}
+
+var car = {
+    engine: {
+        belt: "4hg5k",
+        sparkplugs: ['ere33g', 'ere33g', 'ere33g', 'ere33g'],
+    },
+    wheel: {
+        horn: "loud",
+    }
+};
+console.log("--------------------------------------");
+for (var part in car) {
+    //document.getElementById('mycar').innerHTML = "<h2>" + part + "</h2>";
+    console.log(part);
+    console.log(car[part]);
+    for ( var component in car[part]) {
+        console.log(component);
+        console.log(car[part][component]);
+    }
+}
+
+listOfNumbers.forEach(function (value, index, object) {
+    console.log("Value: " + value);
+    console.log("Index: " + index);
+    console.log(" : " + (value == object));
+    console.log("Object Index: " + object[index]);
+    console.log("Object: " + object);
+});
+
+var done = false;
+
+while (!done && times > 0) {
+
+}
