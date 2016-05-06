b9d2314d5fdded4d6e9e01e5fcf3e9b39d2c907f
Isaac <theicean@gmail.com>
2016-05-06 09:18:28 -0600

    Switch statments

---
+++ src/switch.js
@@ -0,0 +1,14 @@
+/**
+ * Created by isaacwatts on 5/6/16.
+ */
+function Myfunction(myID) {
+    timeOfDay = myID;
+    switch (timeOfDay) {
+        case "1pm":
+            alert("Good Afternoon.");
+            break;
+        case "6pm":
+            alert("Good Evening.");
+    }git 
+
+}
