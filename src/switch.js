82a1e92d79c41ee58b9931d19cf63173bdb0d231
Isaac <theicean@gmail.com>
2016-05-06 09:52:15 -0600

    Switch Day

--- src/switch.js
+++ src/switch.js
@@ -9,6 +9,35 @@ function Myfunction(myID) {
             break;
         case "6pm":
             alert("Good Evening.");
-    }git 
+    }git
 
+}
+
+function listDay() {
+    var d = new Date().getDay();
+    var dateIntro = "Today is ";
+
+    switch (d) {
+        case 0:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Sunday";
+            break;
+        case 1:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Monday";
+            break;
+        case 2:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Tuesday";
+            break;
+        case 3:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Wednesday";
+            break;
+        case 4:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Thursday";
+            break;
+        case 5:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Friday";
+            break;
+        case 6:
+            document.getElementById("dayofweek").innerHTML = dateIntro + "Saturday";
+            break;
+    }
 }
