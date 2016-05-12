95f4f71f998b9626ccbcf871c474de9bf23cff06
Isaac <theicean@gmail.com>
2016-05-12 09:17:50 -0600

    Stings and promises

--- src/js/functions.js
+++ src/js/functions.js
@@ -17,5 +17,48 @@ addItem();
 addItem();
 addItem();
 
-console.log(addItem());
-console.log(addItem());
+//console.log(addItem());
+//console.log(addItem());
+
+var dieres;
+
+function diceToss() {
+    dieres = Math.floor(Math.random() * 20) + 1;
+
+    return dieres;
+}
+
+var promise = new Promise(function (fufill, reject) {
+
+    var die = diceToss();
+
+    if (die === 20) {
+        fufill(die);
+    } else {
+        reject(die);
+    }
+
+    greeting('Isaac');
+
+});
+
+
+function result() {
+    console.log(dieres);
+    var resultText = "You rolled a " + dieres;
+    //document.getElementById("dice_result").innerHTML += resultText;
+}
+
+promise.then(function (toss) {
+
+    var win = "Critical Hit! You rolled a " + toss;
+    console.log(win);
+    result();
+
+}, function (toss) {
+
+    var reg = "You rolled a " + toss;
+    console.log(reg);
+    result();
+
+});
