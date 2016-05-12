d6d1470d1b3c35a34f94d9f88ff294cd56bfd753
Isaac <theicean@gmail.com>
2016-05-12 10:16:29 -0600

    String stuff and arrays

--- src/stringcode.js
+++ src/stringcode.js
@@ -3,18 +3,48 @@
  */
 
 // JavaScript Document
-var currentstring = '';
+var currentstring = [];
+var fullString = '';
+var i = 0;
+
+function destroyletter(el) {
+    el.parentNode.removeChild(el);
+
+    // need to update current string
+    var remClass = el.className;
+    //console.log(remClass);
+    var remClass = remClass.replace(/[^0-9\.]/g, '');
+    //console.log(remClass);
+
+    if (remClass > -1) {
+        currentstring.splice(remClass, 1);
+    }
+
+
+    checkmyAnswer();
+}
 
 function sendLetter(theletter) {
     var outputcontdiv = document.getElementById("output");
-    currentstring += theletter; // We need to concatenate the strings together.
-    checkmyAnswer(currentstring);
-    outputcontdiv.innerHTML = currentstring; // I want you to make it print to the page in lower case.
+    currentstring.push( "<span class='letter" + i + "' onClick='destroyletter(this)'>" + theletter + "</span>" );
+    // We need to concatenate the strings together.
+    fullString = currentstring.join();
+    fullString = fullString.replace( /,/g, "" );
+
+    console.log(fullString);
+
+    checkmyAnswer();
+    outputcontdiv.innerHTML = fullString;
+    // I want you to make it print to the page in lower case.
+
+    i++;
 
 }
 
-function checkmyAnswer(instring) {
-    var otheranswer = "RING";  // Lets pull another word out of the word we want them to spell.
+function checkmyAnswer() {
+    var instring = document.getElementById("output").innerText;
+    console.log(instring);
+    var otheranswer = instring.substring(2, 6);  // Lets pull another word out of the word we want them to spell.
     if(instring === "STRING"){
         alert("you win! String also contains the word " + otheranswer);
     }
