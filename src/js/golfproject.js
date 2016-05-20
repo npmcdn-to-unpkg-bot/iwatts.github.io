605e1132e9cdb6148e4b7ca1f2da0115d0da9c2f
Isaac <theicean@gmail.com>
2016-05-20 08:34:02 -0600

    Golf title

--- src/js/golfproject.js
+++ src/js/golfproject.js
@@ -1,42 +1,39 @@
-var numplayers = 5;
+var numplayers = 6;
+var numholes = 18;
+var holeList = "";
 
 function runcode() {
-    if ($(window).width() < 1174) {
-        errorCheck("small");
+    var titleRow = "<div class='column column-title'><div class='title-players'>Players</div></div>";
+    $("#scorecard").append(titleRow);
+    for(var h = 1; h <= numholes; h++) {
+        var holeTitle = "<div class='title-hole'>" + h + "</div>";
+        holeList += holeTitle;
     }
+    $(".column-title").append(holeList);
     for(var p = 1; p <= numplayers; p++ ){
         var playerRow = "<div class='column pc-" + p + "'><input type='text' value='player" + p + "'></div>";
         $("#scorecard").append(playerRow);
         collectholes(p);
     }
 }
-var resizeTimer;
-
-$(window).resize(function() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout( function() {
-        if ($(window).width() < 1174) {
-            errorCheck("small");
-        }
-    }, 300);
-});
 
 function collectholes(player){
     var golfcourse = "";
-    for(var h = 1; h <= 18; h++){
+    for(var h = 1; h <= numholes; h++){
         var hole = "<div id='player" + player +"hole" + h +"'><input onkeypress='validate(event)' type='text' value=''></div>";
         golfcourse += hole;
         var apTo = ".pc-" + player;
-        //console.log(golfcourse);
+
     }
     $(apTo).append(golfcourse);
+
 }
 
 function validate(evt) {
     var theEvent = evt || window.event;
     var key = theEvent.keyCode || theEvent.which;
     key = String.fromCharCode( key );
-    var regex = /[0-9]/;
+    var regex = /[0-9-]/;
     if( !regex.test(key) ) {
         theEvent.returnValue = false;
         alert("Please enter a Numeric Value.");
@@ -44,14 +41,14 @@ function validate(evt) {
     }
 }
 
-function errorCheck(data) {
-    try {
-        windowSize(data);
-    }
-    catch (error){
-        alert("The following error was handled: " + error);
-    }
+function playerCount(numChange) {
+    numplayers += numChange;
+    $("#scorecard").empty();
+    runcode();
 }
-function windowSize(data) {
-    if (data == "small") throw " Browser is too small";
+
+function holeCount(numChange) {
+    numholes = numChange;
+    $("#scorecard").empty();
+    runcode();
 }
