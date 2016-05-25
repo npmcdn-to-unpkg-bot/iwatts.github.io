ddec31cf1afb54772301e1174311a4ca170c48ae
Isaac <theicean@gmail.com>
2016-05-25 09:43:28 -0600

    Maps API added

--- src/js/golfproject.js
+++ src/js/golfproject.js
@@ -1,8 +1,13 @@
 var numplayers = 6;
 var numholes = 18;
 var myTime = setInterval(function () { myTimer(), 1000});
-
+var map;
+var myLat = 0;
+var myLng = 0;
+var defZoom = 1;
+var url2= "http://api.openweathermap.org/data/2.5/weather?id=5780026&appid=20e833c9715665014beb18e4e9f50aa5";
 var xmlhttp = new XMLHttpRequest();
+
 xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var myObj = JSON.parse(xmlhttp.responseText);
@@ -12,22 +17,9 @@ xmlhttp.onreadystatechange = function() {
         document.getElementById("weather").innerHTML += " " + myObj.name + ": " + str;
     }
 };
-
-var url2= "http://api.openweathermap.org/data/2.5/weather?id=5780026&appid=20e833c9715665014beb18e4e9f50aa5";
-
 xmlhttp.open("GET", url2, true);
 xmlhttp.send();
 
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' +
-            arr[i].display + '</a><br>';
-    }
-    //document.getElementById("id01").innerHTML = out;
-    console.log(out);
-}
 
 function runcode() {
     var titleRow = "<div class='column column-title'><div class='title-players'>Players</div></div>";
@@ -68,8 +60,6 @@ function validate() {
     var self = $(event.target);
     var scoreVal = parseInt(self.val());
     var selfId = self.parent().attr('id');
-    //console.log(scoreVal);
-    //console.log(selfId);
     updateScore(selfId, scoreVal);
 }
 
@@ -108,3 +98,46 @@ function updateScore(playerID, scoreImp) {
     }
 
 }
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    myLat = position.coords.latitude;
+    myLng = position.coords.longitude;
+    defZoom = 12;
+    initMap();
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: myLat, lng: myLng},
+        zoom: defZoom
+    });
+    smoothZoom(map, defZoom, map.getZoom());
+    var image = '../src/img/tee-off.png';
+    var teeoff = new google.maps.Marker({
+        position: {lat: 40.233, lng: -111.658},
+        map: map,
+        icon: image
+    });
+}
+function smoothZoom (map, max, cnt) {
+    if (cnt >= max) {
+        return;
+    }
+    else {
+        z = google.maps.event.addListener(map, 'zoom_changed', function(event){
+            google.maps.event.removeListener(z);
+            smoothZoom(map, max, cnt + 1);
+        });
+        setTimeout(function(){map.setZoom(cnt)}, 80); // 80ms is what I found to work well on my system -- it might not work well on all systems
+    }
+}
+getLocation();
+
