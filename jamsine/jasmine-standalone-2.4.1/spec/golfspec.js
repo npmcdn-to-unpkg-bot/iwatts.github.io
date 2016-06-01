3d813d7ea7bbf84a1a0145f6307c595a81627c8e
Isaac <theicean@gmail.com>
2016-06-01 10:06:12 -0600

    Golfspec test

---
+++ jamsine/jasmine-standalone-2.4.1/spec/golfspec.js
@@ -0,0 +1,21 @@
+describe("Stack implement", function () {
+    describe('push', function () {
+        it('increases size of stack', function () {
+            
+        });
+        it('succeeds on num input', function () {
+
+        });
+    });
+    describe('pop', function () {
+        
+    });
+    describe('size', function () {
+        
+    });
+    describe('isEmpty', function () {
+        
+    });
+    
+    
+});
