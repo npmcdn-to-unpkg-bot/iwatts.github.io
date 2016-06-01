dbca7c9c921c41fe862d777eb1a054608802a79c
Isaac <theicean@gmail.com>
2016-06-01 10:21:24 -0600

    Stack tests with Jasmine

--- jamsine/jasmine-standalone-2.4.1/spec/stack-test.js
+++ jamsine/jasmine-standalone-2.4.1/spec/stack-test.js
@@ -1,21 +1,33 @@
 describe("Stack implement", function () {
+
+    var stack;
+
+    beforeEach(function () {
+        stack = new Stack();
+    });
+
     describe('push', function () {
         it('increases size of stack', function () {
-            
+            stack.push(4521);
+            expect(stack.size()).toEqual(1);
+
         });
         it('succeeds on num input', function () {
 
         });
+        it('fails on NaN', function () {
+
+        });
     });
     describe('pop', function () {
-        
+
     });
     describe('size', function () {
-        
+
     });
     describe('isEmpty', function () {
-        
+
     });
-    
-    
+
+
 });
---
+++ src/js/stack.js
@@ -0,0 +1,11 @@
+/**
+ * Created by isaacwatts on 6/1/16.
+ */
+function Stack() {
+    this.push = function () {
+        
+    }
+    this.size = function () {
+        
+    }
+}
