adae3c0e2bcd5100d5d8240ccf7982affe1cc54f
iwatts <isaac@elikirk.com>
2016-06-02 16:30:19 -0600

    Ussing api with ssl v2

--- src/js/golfproject.js
+++ src/js/golfproject.js
@@ -72,9 +72,9 @@ function weatherDisplay() {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var myObj = JSON.parse(xmlhttp.responseText);
-            var str = myObj.weather[0].description.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+            /*var str = myObj.weather[0].description.toLowerCase().replace(/\b[a-z]/g, function(letter) {
                 return letter.toUpperCase();
-            });
+            });*/
 			console.log(myObj);
             document.getElementById("weather").innerHTML = "Weather Conditions in " + myObj.name + ":<br>" + str;
         }
