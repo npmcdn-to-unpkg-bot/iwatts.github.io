a852652895e6ea0897415f28bffb387165833825
Isaac <theicean@gmail.com>
2016-06-02 10:05:38 -0600

    Jasmine testing implementd

--- jamsine/jasmine-standalone-2.4.1/spec/stack-test.js
+++ jamsine/jasmine-standalone-2.4.1/spec/stack-test.js
@@ -13,19 +13,110 @@ describe("Stack implement", function () {
 
         });
         it('succeeds on num input', function () {
-
+            var success = stack.push(4522);
+            expect(success).toBeTruthy();
+            expect(stack.size()).toEqual(1);
         });
-        it('fails on NaN', function () {
-
+        it('fails on NaN:string', function () {
+            var success = stack.push("apple");
+            expect(success).toBeFalsy();
+            expect(stack.size()).toEqual(0);
+        });
+        it('fails on NaN:object', function () {
+            var success = stack.push({data:12654});
+            expect(success).toBeFalsy();
+            expect(stack.size()).toEqual(0);
+        });
+        it('fails on NaN:array', function () {
+            var success = stack.push(["apple"],123, true);
+            expect(success).toBeFalsy();
+            expect(stack.size()).toEqual(0);
+        });
+        it('fails on NaN:function', function () {
+            var success = stack.push( function () {
+                alert("test");
+            });
+            expect(success).toBeFalsy();
+            expect(stack.size()).toEqual(0);
+        });
+        it('fails on negative input', function () {
+            var success = stack.push(-8765);
+            expect(success).toBeFalsy();
+            expect(stack.size()).toEqual(0);
+        });
+        it('fails on decimal input', function () {
+            var success = stack.push(32.056);
+            expect(success).toBeFalsy();
+            expect(stack.size()).toEqual(0);
         });
     });
     describe('pop', function () {
 
+        var number1 = 1;
+        var number2 = 2;
+        var number3 = 3;
+
+        beforeEach( function () {
+            stack.push(number1);
+            stack.push(number2);
+            stack.push(number3);
+        });
+
+        it('should increase the size of the stack correctly', function () {
+            var number = stack.pop();
+            expect(stack.size()).toEqual(2);
+        });
+
+        it('should pop correct sequence', function () {
+            var firstNum = stack.pop();
+            var secondNum = stack.pop();
+            var thirdNum = stack.pop();
+
+            expect(firstNum).toEqual(number3);
+            expect(secondNum).toEqual(number2);
+            expect(thirdNum).toEqual(number1);
+        });
+        it('should return undefined when empty', function () {
+            stack = new Stack();
+            expect(stack.size()).toEqual(0);
+
+            var number = stack.pop();
+
+            expect(number).toEqual(undefined);
+        });
+
     });
     describe('size', function () {
 
+        it('should return the right size', function () {
+
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            expect(stack.size()).toEqual(3);
+
+            stack.push(75395);
+
+            expect(stack.size()).toEqual(4);
+
+        });
+
     });
     describe('isEmpty', function () {
+
+        it('should return true when empty', function () {
+            expect(stack.size()).toEqual(0);
+            expect(stack.isEmpty()).toBeTruthy();
+        });
+        it('should return false when not empty', function () {
+
+            stack.push(149);
+
+            expect(stack.size()).toEqual(1);
+            expect(stack.isEmpty()).toBeFalsy();
+
+        });
 
     });
 
--- src/js/stack.js
+++ src/js/stack.js
@@ -2,10 +2,33 @@
  * Created by isaacwatts on 6/1/16.
  */
 function Stack() {
-    this.push = function () {
-        
-    }
+
+    var container = [];
+
+    this.push = function (number) {
+
+        if(typeof number == 'number' && number >= 0 && Math.floor(number) == number) {
+            container.push(number);
+            console.log(container);
+
+            return true;
+        }
+
+        return false;
+
+    };
+
     this.size = function () {
-        
-    }
+        return container.length;
+    };
+
+    this.pop = function () {
+        var number = container.pop();
+        console.log(container);
+        return number;
+    };
+
+    this.isEmpty = function () {
+        return container.length == 0;
+    };
 }
