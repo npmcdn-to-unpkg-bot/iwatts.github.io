fe3cd2e0c588d9c62d5e379268ee472b6618b88b
Isaac <theicean@gmail.com>
2016-06-24 09:42:34 -0600

    Mine positioning

--- src/js/flappygame.js
+++ src/js/flappygame.js
@@ -151,7 +151,7 @@ function MineCollection() {
 
 function Mine() {
     this.x = 500;
-    this.y = height - (mineSprite.height + foregroundSprite.height + 100 + 200 * Math.random());
+    this.y = height - (mineSprite.height + foregroundSprite.height + 90 + 200 * Math.random());
     this.width = mineSprite.width;
     this.height = mineSprite.height;
 
